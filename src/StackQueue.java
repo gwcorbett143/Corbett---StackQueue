f08ba663cc21a05e911d608ef2041fe565970888
gwcorbett143 <gcorbett23@kentdenver.org>
2023-01-11 11:04:59 -0700

    StackQueueNode File added and StackQueue methods added

--- src/StackQueue.java
+++ src/StackQueue.java
@@ -1,4 +1,52 @@
-public class StackQueue {
+public class StackQueue <N> {
 
+    public static class Node<Item>{
+        private Item data;
+        private Node<Item> next;
+
+        public Node(Item data) {
+            this.data = data;
+        }
+
+    }
+
+    private StackNode inStack = new StackNode();
+    private StackNode outStack = new StackNode();
+
+    private Node<Item> head;
+    private Node<Item> tail;
+
+    public boolean isEmpty(){
+        return head == null;
+    }
+
+    public Item peek(){
+        return head.data;
+    }
+
+    public void enqueue(Item data){
+        Node<Item> node = new Node<Item>(data);
+        if (tail != null){
+            tail.next = node;
+        }
+        tail = node;
+
+        if (head == null){
+            head = node;
+        }
+    }
+
+    public Item dequeue(){
+        Item data = head.data;
+        head = head.next;
+
+        if(head == null){
+            tail = null;
+        }
+        return data;
+    }
+}
+
+    }
 
 }
